2aeeb3f9ed45c8cb9fbe880703852d446723bf62
BC Huff <bhuff4050@myhc.us>
2015-10-22 13:39:39 -0500

    Update hello.cpp
    Updated to include C++14 standard example along side the C++11 standard example.

--- helloworld/hello.cpp
+++ helloworld/hello.cpp
@@ -1,8 +1,14 @@
 //hello world
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+  //This example by Josh is using C++11 standard
   std::cout << "Hello World!" << std::endl;
+  
+  //This example by Bryan is using C++14 standard
+  cout << "Hello World!" << endl;
   return 0;
 }
